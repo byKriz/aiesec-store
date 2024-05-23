fe018ef70a458d159dd58f40978fdeffd6771785
cristianesquivelh <cristianesquivelh@gmail.com>
2024-05-23 22:53:56 +0000

    agregano la seccion del banner

---
+++ src/consts/bannerName.ts
@@ -0,0 +1,8 @@
+export enum BannerName {
+    LIDERAZGO = 'Liderazgo',
+    EXCELENCIA = 'Excelencia',
+    INTEGRIDAD = 'Integridad',
+    PARTICIPACION = 'Participación',
+    DIVERSIDAD = 'Diversidad',
+    SOSTENIBILIDAD = 'Sostenibilidad',
+}
