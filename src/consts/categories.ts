9e7796fb34a6e872f128bbc1ca216b009617b993
cristianesquivelh <cristianesquivelh@gmail.com>
2024-06-13 06:05:40 +0000

    ajustes menores

---
+++ src/consts/categories.ts
@@ -0,0 +1,5 @@
+export enum Categories {
+    FRANELAS = 'FRANELAS',
+    STICKERS = 'STICKERS',
+
+}
---
+++ src/models/Product.ts
@@ -0,0 +1,12 @@
+import { Categories } from "../consts/categories";
+
+export interface Product {
+    id: number;
+    name: string;
+    description: string;
+    category: Categories;
+    price: number;
+    image: string;
+}
+
+
